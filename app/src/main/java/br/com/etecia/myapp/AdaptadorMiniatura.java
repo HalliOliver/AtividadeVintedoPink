5d1e30eed96a6afc41543d75349d68d3b19161dc
hallioliver <hallison_vida@hotmail.com>
2025-08-29 22:16:14 -0300

    Hallison fazendo perguntas: Tom: -_-

--- app/src/main/java/br/com/etecia/myapp/AdaptadorMiniatura.java
+++ app/src/main/java/br/com/etecia/myapp/AdaptadorMiniatura.java
@@ -1,4 +1,43 @@
 package br.com.etecia.myapp;
 
-public class AdaptadorMiniatura {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.security.PublicKey;
+import java.util.List;
+public class AdaptadorMiniatura extends RecyclerView.Adapter<AdaptadorMiniatura.ViewHolder> {
+
+    private Context context;
+
+    private List<Miniatura> lstminiatura;
+
+    public AdaptadorMiniatura(Context context, List<Miniatura> lstminiatura) {
+        this.context = context;
+        this.lstminiatura = lstminiatura;
+
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.modelo_miniatura_layout, parent, false);
+        return new ViewHolder(view);
+    }
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+    public ViewHolder(@NonNull View itemView){
+        super(itemView);
+        cardMiniatura = itemView.findViewById(R.id.cardMiniaturas);
+
+    }
 }
--- app/src/main/java/br/com/etecia/myapp/Miniatura.java
+++ app/src/main/java/br/com/etecia/myapp/Miniatura.java
@@ -1,8 +1,30 @@
 package br.com.etecia.myapp;
 
 public class Miniatura {
-    private int imgMiniatura;
-    private int tituloMiniatura;
+    private String titulo;
+    private int imagem;
 
+    //criando o construtor
 
+    public Miniatura(String titulo, int imagem) {
+        this.titulo = titulo;
+        this.imagem = imagem;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(int imagem) {
+        this.imagem = imagem;
+    }
 }
+
